f0be3df89721bd14f0146b1b07f3c2d5564929c8
jshn9515 <jshn9515@163.com>
2024-10-28 14:44:39 +0800

    Rearrange project structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from d2l import __version__
+
+setuptools.setup(
+    name='d2l',
+    version=__version__,
+    description='A lightweight deep learning library',
+    author='jshn9515',
+    author_email='jshn9515@163.com',
+    packages=setuptools.find_packages(),
+    requires=['numpy', 'matplotlib', 'torch'],
+)
